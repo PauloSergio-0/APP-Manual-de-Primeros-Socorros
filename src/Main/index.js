a6727d2edef540baeeccd037aa8ed6e4769fcf0e
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-11-14 15:45:11 -0300

    added database

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,5 +1,5 @@
-import { Text } from "react-native";
-import { useState } from 'react';
+import { Text, Alert } from "react-native";
+import { useState , useEffect} from 'react';
 
 import { Container } from "./styles";
 import Header from "../components/Header";
@@ -14,32 +14,51 @@ export default function Main() {
   const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
   const [isNewGuiaModalVisible, setIsNewGuiaModalVisible] = useState(false);
   const [isEditGuiaModalVisible, setIsEditGuiaModalVisible] = useState(false);
-  const [guiaBeingEdit, setGuiaBeingEdit] = useState('');
+  const [guiaBeingEdit, setGuiaBeingEdit] = useState(null);
 
-  function handleChangeStatus(guia) {
-    alert("Alterar Status da Tarefa");
+// Função de edição de guia
+function handleEditGuia(guia) {
+  console.log("Antes de alterar:", guia);  // Verifica o guia antes de alterá-lo
+  setGuiaBeingEdit(guia);  // Atualiza o estado
+
+  // Não precisa acessar o estado imediatamente após a atualização
+  // O estado será atualizado na próxima renderização do componente
+  setIsEditGuiaModalVisible(true);
+}
+
+  // UseEffect para monitorar mudanças no estado guiaBeingEdit
+  useEffect(() => {
+    console.log("state atualizado:", guiaBeingEdit);
+  }, [guiaBeingEdit]);
+
+  // Função para salvar edição da guia
+  function handleSaveEditGuia(updatedGuia) {
+    if (updatedGuia.title) {
+      console.log("Após edição:", updatedGuia);  // Verifica o guia após a edição
+      console.log(`titulo: ${updatedGuia.title}\nDescrição: ${updatedGuia.description}`);
+      setIsEditGuiaModalVisible(false);
+    } else {
+      console.error("Guia inválida");
+    }
   }
 
-  function handleEditGuia(guia) {
-    setGuiaBeingEdit(guia);
-    setIsEditGuiaModalVisible(true);
-  }
 
+  // Função para confirmar a exclusão da guia
   function handleConfirmDeleteGuia(guia) {
     setIsDeleteModalVisible(true);
   }
 
+  // Função para confirmar a exclusão
   function handleDeleteGuia() {
     setIsDeleteModalVisible(false);
   }
 
+  // Função para criar nova guia
   function handleCreateGuia(guia) {
     setIsNewGuiaModalVisible(false);
   }
 
-  function handleSaveEditGuia(guia) {
-    setIsEditGuiaModalVisible(false);
-  }
+
 
   return (
     <Container>
@@ -49,27 +68,31 @@ export default function Main() {
         guia={guia}  // Passando os dados corretamente
         onEditGuia={handleEditGuia}
         onDeleteGuia={handleConfirmDeleteGuia}
+        
         DeleteGuia={handleConfirmDeleteGuia}
-        />
+      />
 
-        <AddButton onPress={() => setIsNewGuiaModalVisible(true)} />
+      <AddButton onPress={() => setIsNewGuiaModalVisible(true)} />
+      
       <DeleteConfirmModal
           visible={isDeleteModalVisible}
           onClose={() => setIsDeleteModalVisible(false)}
           onConfirm={handleDeleteGuia}
         />
-        <NewGuiaModal
+      
+      <NewGuiaModal
         visible={isNewGuiaModalVisible}
-        onClose={() => setIsNewGuiaModalVisible(false)}
         onSave={handleCreateGuia}
+        onClose={() => setIsNewGuiaModalVisible(false)}
       />
 
       <EditGuiaModal
         visible={isEditGuiaModalVisible}
         onClose={() => setIsEditGuiaModalVisible(false)}
         onSave={handleSaveEditGuia}
-        guia={guiaBeingEdit}
+        guia={guiaBeingEdit} // Passando o guia sendo editado
       />
+
     </Container>
   );
 }
---
+++ src/services/GuiaService.js
@@ -0,0 +1,79 @@
+import { db } from './SQLite';
+
+export function createTable() {
+  const sql = `CREATE TABLE IF NOT EXISTS guias (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    create_date TIMESTAMP);
+    `;
+
+  db.transaction((transaction) => {
+    transaction.executeSql(sql);
+  }, (error) => {
+    console.log("error call back : " + JSON.stringify(error));
+    console.log(error);
+  }, () => {
+    console.log("transaction complete call back ");
+  });
+}
+
+export async function createGuia(Guia) {
+  return new Promise((resolve) => {
+    db.transaction((transaction) => {
+      transaction.executeSql("INSERT INTO guias (title, description, create_date) VALUES (?, ?,  datetime());", [guia.title, guia.description], () => {
+        resolve("Tarefa adicionada com sucesso!");
+      })
+    }, (error) => {
+      console.log("error call back : " + JSON.stringify(error));
+      console.log(error);
+    }, () => {
+      console.log("transaction complete call back ");
+    });
+  })
+}
+
+export async function updateGuia(guia) {
+  return new Promise((resolve) => {
+    db.transaction((transaction) => {
+      transaction.executeSql("UPDATE guias SET title = ?, description = ? WHERE id = ?;", [guia.title, guia.description, guia.id], () => {
+        resolve("Tarefa atualizada com sucesso!");
+      })
+    }, (error) => {
+      console.log("error call back : " + JSON.stringify(error));
+      console.log(error);
+    }, () => {
+      console.log("transaction complete call back ");
+    });
+  })
+}
+
+export async function deleteGuia(guia) {
+  return new Promise((resolve) => {
+    db.transaction((transaction) => {
+      transaction.executeSql("DELETE FROM guias WHERE id = ?;", [guia.id], () => {
+        resolve("Tarefa removida com sucesso!");
+      })
+    }, (error) => {
+      console.log("error call back : " + JSON.stringify(error));
+      console.log(error);
+    }, () => {
+      console.log("transaction complete call back ");
+    });
+  })
+}
+
+export async function listGuias() {
+  return new Promise((resolve) => {
+    db.transaction((transaction) => {
+      transaction.executeSql("SELECT * FROM guias ORDER BY create_date DESC;", [], (transaction, resultado) => {
+        resolve(resultado.rows._array);
+      })
+    }, (error) => {
+      console.log("error call back : " + JSON.stringify(error));
+      console.log(error);
+    }, () => {
+      console.log("transaction complete call back ");
+    });
+  })
+}
